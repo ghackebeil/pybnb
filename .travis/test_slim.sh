9b51bf0be7a0e9a0778029ef0375578a4c61c4a3
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-26 23:58:13 -0400

    cleanup coverage collection

--- .travis/test_slim.sh
+++ .travis/test_slim.sh
@@ -7,9 +7,5 @@ ${DOC_} pip uninstall -y Pyomo
 ${DOC_} pip uninstall -y Pyutilib
 ${DOC_} pip uninstall -y numba
 ${DOC_} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
-${DOC_} mv .coverage ./.coverage.1
 ${DOC_} pip install Pyomo
 ${DOC_} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
-${DOC_} mv .coverage ./.coverage.2
-${DOC_} coverage combine
-${DOC_} coverage report
