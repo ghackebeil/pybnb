7e217f533fe4a6a6bbc13243d95314e3536da9bc
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-27 14:18:32 -0400

    prevent coverage files from being merged until the end so we can keep track of things easier

--- .travis/test_slim.sh
+++ .travis/test_slim.sh
@@ -7,12 +7,11 @@ ${DOC_} pip uninstall -y Pyomo
 ${DOC_} pip uninstall -y Pyutilib
 ${DOC_} pip uninstall -y numba
 ${DOC_} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
-${DOC_} find . -name "*.coverage*"
-${DOC_} mv .coverage .coverage.slim.1
-${DOC_} find . -name "*.coverage*"
+${DOC_} mv .coverage coverage.slim.1
 ${DOC_} pip install Pyomo
-${DOC_} find . -name "*.coverage*"
 ${DOC_} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
-${DOC_} find . -name "*.coverage*"
-${DOC_} mv .coverage .coverage.slim.2
-${DOC_} find . -name "*.coverage*"
+${DOC_} mv .coverage coverage.slim.2
+# prepare the coverage files for the "coverage combine"
+# call that comes next
+${DOC_} mv coverage.slim.1 .coverage.slim.1
+${DOC_} mv coverage.slim.2 .coverage.slim.2
