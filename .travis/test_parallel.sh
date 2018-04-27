9748562ccd0f345d0d1ca5ed49473d6af052b409
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-27 01:00:29 -0400

    fixing coverage report

--- .travis/test_parallel.sh
+++ .travis/test_parallel.sh
@@ -5,5 +5,8 @@ set -e
 EXAMPLE_ARG=`[[ -z $EXAMPLES ]] || echo --run-examples`
 ${DOC_} pytest -v --doctest-modules src/pybnb
 ${DOC_} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" -v ${EXAMPLE_ARG}
+${DOC_} mv .coverage .coverage.parallel.1
 ${DOC_} python run-mpitests.py --no-build --with-coverage -v
+${DOC_} mv .coverage .coverage.parallel.2
 ${DOC_} python run-mpitests.py --single --no-build --with-coverage -v
+${DOC_} mv .coverage .coverage.parallel.3
--- .travis/test_slim.sh
+++ .travis/test_slim.sh
@@ -7,5 +7,7 @@ ${DOC_} pip uninstall -y Pyomo
 ${DOC_} pip uninstall -y Pyutilib
 ${DOC_} pip uninstall -y numba
 ${DOC_} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
+${DOC_} mv .coverage .coverage.slim.1
 ${DOC_} pip install Pyomo
 ${DOC_} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
+${DOC_} mv .coverage .coverage.slim.2
