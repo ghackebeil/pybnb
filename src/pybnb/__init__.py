0cf47ead325da5a689323cb7f610899efa6a8282
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-27 17:26:27 -0400

    clean up init

--- src/pybnb/__init__.py
+++ src/pybnb/__init__.py
@@ -2,12 +2,13 @@ from pybnb.__about__ import __version__
 
 import pybnb.mpi_utils
 import pybnb.misc
-from pybnb.misc import infinity
 import pybnb.problem
-from pybnb.problem import (Problem,
-                             minimize,
-                             maximize)
 import pybnb.dispatcher_proxy
 import pybnb.dispatcher
 import pybnb.solver
+
+from pybnb.misc import infinity
+from pybnb.problem import (Problem,
+                           minimize,
+                           maximize)
 from pybnb.solver import Solver
