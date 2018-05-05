aafe46dd5e9c2176824862a18892667e74e511f8
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-05-05 12:19:15 -0400

    fixing travis

--- .travis/test_parallel.sh
+++ .travis/test_parallel.sh
@@ -6,9 +6,9 @@ EXAMPLE_ARG=`[[ -z $EXAMPLES ]] || echo --run-examples`
 ${DOC} pytest -v --doctest-modules src/pybnb
 ${DOC} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" -v ${EXAMPLE_ARG}
 ${DOC} mv .coverage coverage.parallel.1
-${DOC} python run-mpitests.py --no-build --with-coverage -v
+${DOC} python run-mpitests.py -s --no-build --with-coverage -v
 ${DOC} mv .coverage coverage.parallel.2
-${DOC} python run-mpitests.py --single --no-build --with-coverage -v
+${DOC} python run-mpitests.py -s --single --no-build --with-coverage -v
 ${DOC} mv .coverage coverage.parallel.3
 # prepare the coverage files for the "coverage combine"
 # call that comes next
