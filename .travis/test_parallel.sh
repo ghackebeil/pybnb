1975ba7112e42ae70ef4f2bdc055dedcdd359f98
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-05-05 11:28:20 -0400

    fix install scripts

--- .travis/test_parallel.sh
+++ .travis/test_parallel.sh
@@ -3,15 +3,15 @@
 set -e
 
 EXAMPLE_ARG=`[[ -z $EXAMPLES ]] || echo --run-examples`
-${DOC_} pytest -v --doctest-modules src/pybnb
-${DOC_} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" -v ${EXAMPLE_ARG}
-${DOC_} mv .coverage coverage.parallel.1
-${DOC_} python run-mpitests.py --no-build --with-coverage -v
-${DOC_} mv .coverage coverage.parallel.2
-${DOC_} python run-mpitests.py --single --no-build --with-coverage -v
-${DOC_} mv .coverage coverage.parallel.3
+${DOC} pytest -v --doctest-modules src/pybnb
+${DOC} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" -v ${EXAMPLE_ARG}
+${DOC} mv .coverage coverage.parallel.1
+${DOC} python run-mpitests.py --no-build --with-coverage -v
+${DOC} mv .coverage coverage.parallel.2
+${DOC} python run-mpitests.py --single --no-build --with-coverage -v
+${DOC} mv .coverage coverage.parallel.3
 # prepare the coverage files for the "coverage combine"
 # call that comes next
-${DOC_} mv coverage.parallel.1 .coverage.parallel.1
-${DOC_} mv coverage.parallel.2 .coverage.parallel.2
-${DOC_} mv coverage.parallel.3 .coverage.parallel.3
+${DOC} mv coverage.parallel.1 .coverage.parallel.1
+${DOC} mv coverage.parallel.2 .coverage.parallel.2
+${DOC} mv coverage.parallel.3 .coverage.parallel.3
--- .travis/test_slim.sh
+++ .travis/test_slim.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-${DOC_} pip uninstall -y mpi4py
-${DOC_} pip uninstall -y Pyomo
-${DOC_} pip uninstall -y Pyutilib
-${DOC_} pip uninstall -y numba
-${DOC_} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
-${DOC_} mv .coverage coverage.slim.1
-${DOC_} pip install Pyomo
-${DOC_} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
-${DOC_} mv .coverage coverage.slim.2
+${DOC} pip uninstall -y mpi4py
+${DOC} pip uninstall -y Pyomo
+${DOC} pip uninstall -y Pyutilib
+${DOC} pip uninstall -y numba
+${DOC} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
+${DOC} mv .coverage coverage.slim.1
+${DOC} pip install Pyomo
+${DOC} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
+${DOC} mv .coverage coverage.slim.2
 # prepare the coverage files for the "coverage combine"
 # call that comes next
-${DOC_} mv coverage.slim.1 .coverage.slim.1
-${DOC_} mv coverage.slim.2 .coverage.slim.2
+${DOC} mv coverage.slim.1 .coverage.slim.1
+${DOC} mv coverage.slim.2 .coverage.slim.2
