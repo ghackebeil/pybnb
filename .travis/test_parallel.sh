0bd2dad6325c9f1d35ad54e4d39d20425e1e1581
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-05-05 15:00:41 -0400

    fixing travis

--- .travis/test_parallel.sh
+++ .travis/test_parallel.sh
@@ -3,21 +3,20 @@
 set -e
 
 EXAMPLE_ARG=`[[ -z $EXAMPLES ]] || echo --run-examples`
+MPIRUN_FLAG="--mpirun="
 if [ -z "$MPIRUN_ARG" ]
 then
-    if [ "$(${DOC} mpirun --allow-run-as-roots --version 2> /dev/null)" ]
+    if [ "$(${DOC} mpirun --allow-run-as-root --version 2> /dev/null)" ]
     then
-        MPIRUN_ARG='--mpirun="mpirun --allow-run-as-root np 4"'
+        MPIRUN_ARG="mpirun --allow-run-as-root n 4"
     else
-        MPIRUN_ARG='--mpirun="mpirun -np 4"'
+        MPIRUN_ARG="mpirun -n 4"
     fi
 fi
-echo ${MPIRUN_ARG}
 ${DOC} pytest -v --doctest-modules src/pybnb
 ${DOC} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" -v ${EXAMPLE_ARG}
 ${DOC} mv .coverage coverage.parallel.1
-echo python run-mpitests.py ${MPIRUN_ARG} --no-build --with-coverage -v
-${DOC} python run-mpitests.py ${MPIRUN_ARG} --no-build --with-coverage -v
+${DOC} python run-mpitests.py "${MPIRUN_FLAG}""${MPIRUN_ARG}" --no-build --with-coverage -v
 ${DOC} mv .coverage coverage.parallel.2
 ${DOC} python run-mpitests.py --single --no-build --with-coverage -v
 ${DOC} mv .coverage coverage.parallel.3
