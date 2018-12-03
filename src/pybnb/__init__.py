51cecafcd6e107c8d385b93fde6f14e72c075703
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-12-03 15:14:37 -0500

    cleanup __init__

--- src/pybnb/__init__.py
+++ src/pybnb/__init__.py
@@ -1,16 +1,4 @@
 from pybnb.__about__ import __version__
-
-import pybnb.misc
-import pybnb.common
-import pybnb.node
-import pybnb.problem
-import pybnb.convergence_checker
-import pybnb.mpi_utils
-import pybnb.priority_queue
-import pybnb.dispatcher_proxy
-import pybnb.dispatcher
-import pybnb.solver
-
 from pybnb.common import (minimize,
                           maximize,
                           inf,
