c953ab2fed8d22138884364b7fe56f3ee1296c48
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-12-18 16:10:27 -0600

    more tests

--- src/tests/test_pyomo_tools.py
+++ src/tests/test_pyomo_tools.py
@@ -6,7 +6,9 @@ pyomo_available = False
 try:
     import pyomo.kernel as pmo
     from pybnb.pyomo_tools import (_add_tmp_component,
-                                   _create_optimality_bound)
+                                   _create_optimality_bound,
+                                   PyomoProblem,
+                                   RangeReductionProblem)
     pyomo_available = True
 except:
     pass
@@ -57,3 +59,40 @@ class Test(object):
         assert con.ctype is pmo.constraint._ctype
         assert con.body is model.objective
         assert con.ub == 100
+
+    def test_RangeReductionProblem(self):
+        class Junk(PyomoProblem):
+            def __init__(self):
+                self._pyomo_model = pmo.block()
+                self._pyomo_model.x = pmo.variable()
+                self._pyomo_model.c = pmo.constraint()
+                self._pyomo_model.o = pmo.objective()
+                self._pyomo_model_objective = self._pyomo_model.o
+                super(Junk, self).__init__()
+            @property
+            def pyomo_model(self):
+                return self._pyomo_model
+            @property
+            def pyomo_model_objective(self):
+                return self._pyomo_model_objective
+        junk = Junk()
+        assert junk.pyomo_model_objective is junk.pyomo_model.o
+        assert junk.pyomo_object_to_cid[junk.pyomo_model] == ()
+        assert junk.pyomo_object_to_cid[junk.pyomo_model.x] == ('x',)
+        assert junk.pyomo_object_to_cid[junk.pyomo_model.c] == ('c',)
+        assert junk.pyomo_object_to_cid[junk.pyomo_model.o] == ('o',)
+        assert junk.cid_to_pyomo_object[()] is junk.pyomo_model
+        assert junk.cid_to_pyomo_object[('x',)] is junk.pyomo_model.x
+        assert junk.cid_to_pyomo_object[('c',)] is junk.pyomo_model.c
+        assert junk.cid_to_pyomo_object[('o',)] is junk.pyomo_model.o
+        junk.pyomo_model.r = pmo.constraint()
+        junk.update_pyomo_object_cids()
+        assert junk.pyomo_object_to_cid[junk.pyomo_model.r] == ('r',)
+        assert junk.cid_to_pyomo_object[('r',)] is junk.pyomo_model.r
+
+        rr_junk = RangeReductionProblem(junk, pybnb.inf)
+        assert rr_junk.objective() == pybnb.inf
+        rr_junk.notify_new_best_objective_received(None, 1)
+        assert rr_junk.objective() == 1
+        rr_junk.notify_new_best_objective(None, 2)
+        assert rr_junk.objective() == 2
