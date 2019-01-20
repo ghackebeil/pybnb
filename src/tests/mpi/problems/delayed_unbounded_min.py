8849c5103dd58fc5b0f29ee5664ce9279387af44
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2019-01-20 17:46:31 -0500

    forgot to add files to last commit

---
+++ src/tests/mpi/problems/delayed_unbounded_min.py
@@ -0,0 +1,27 @@
+import pybnb
+
+class DelayedUnboundedMin(pybnb.Problem):
+
+    #
+    # Implement Problem abstract methods
+    #
+
+    def sense(self):
+        return pybnb.minimize
+    def objective(self):
+        if self.tree_id >= 10:
+            return self.unbounded_objective()
+        else:
+            return 0
+    def bound(self):
+        return self.unbounded_objective()
+    def save_state(self, node):
+        pass
+    def load_state(self, node):
+        self.tree_id = node.tree_id
+    def branch(self, node):
+        yield node.new_child()
+        yield node.new_child()
+        yield node.new_child()
+        yield node.new_child()
+        yield node.new_child()
