94f84499370cab8fa9b0e55ebac4c4166fbca612
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2019-01-31 19:50:52 -0500

    adding to slim test build

--- .travis/test_slim.sh
+++ .travis/test_slim.sh
@@ -12,7 +12,11 @@ ${DOC} mv .coverage coverage.slim.1
 ${DOC} pip install Pyomo
 ${DOC} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
 ${DOC} mv .coverage coverage.slim.2
+${DOC} pip uninstall -y PyYAML
+${DOC} pytest -v --cov=pybnb --cov=examples --cov=src/tests --cov-report="" --run-examples
+${DOC} mv .coverage coverage.slim.3
 # prepare the coverage files for the "coverage combine"
 # call that comes next
 ${DOC} mv coverage.slim.1 .coverage.slim.1
 ${DOC} mv coverage.slim.2 .coverage.slim.2
+${DOC} mv coverage.slim.3 .coverage.slim.3
