254373fc11b62f8a26eff91c1deb81fa81a371d3
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2019-02-19 19:19:29 -0500

    removing references to python2 in test

--- src/tests/test_package.py
+++ src/tests/test_package.py
@@ -15,10 +15,7 @@ class Test:
     # coverage report includes
     def test_show_coverage(self):
         if not is_pypy:
-            if sys.version_info.major == 2:
-                if sys.version_info.minor == 7:
-                    print(sys.version_info)
-            elif sys.version_info.major == 3:
+            if sys.version_info.major == 3:
                 if sys.version_info.minor == 5:
                     print(sys.version_info)
                 elif sys.version_info.minor == 6:
@@ -26,8 +23,6 @@ class Test:
                 elif sys.version_info.minor == 7:
                     print(sys.version_info)
         if is_pypy:
-            if sys.version_info.major == 2:
-                print(sys.version_info)
             if sys.version_info.major == 3:
                 print(sys.version_info)
 
